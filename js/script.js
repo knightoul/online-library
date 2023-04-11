999aaf50f8c2218a31d4979419f09b42de5273c8
arnab kumar das <arnab8051das@gmail.com>
2023-04-11 07:18:11 +0530

    navbar completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-window.onload = ()=>{
-    console.log("Hello, World!");
-}
+window.onload = () => {
+  console.log("Hello, World!");
+  document.querySelectorAll(".user-portal").forEach((e) => {
+    e.addEventListener("click", (element) => {
+      if (element.target.id === "login") {
+        console.log("login click");
+        /* - TODO Open Login Window - */
+      } else if (element.target.id === "signup") {
+        console.log("signup click");
+        /* - TODO Open Sign Up Window - */
+      }
+    });
+  });
+};
